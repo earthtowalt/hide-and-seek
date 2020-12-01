5415afcce88b89538e350f2757440975dfb20804
earthtowalt <jackwalton00@aol.com>
2020-11-30 22:12:03 -0500

    cleaned up comments, code and added docstrings

--- game.py
+++ game.py
@@ -36,9 +36,9 @@ CLIENT_TO_PROTOCOL = {
 }
 
 
-# Socket
+# Server address
 SERVER_ADDRESS = ('34.224.98.28', 10001)
-#SERVER_ADDRESS = ('172.25.32.1', 10001)
+# SERVER_ADDRESS = ('172.25.32.1', 10001)
 
 # game rules
 COOLDOWN_TIME = 5
@@ -53,22 +53,24 @@ BGGHOST = (100,100,100)
 
 
 class Map:
+    '''Class used to store a map of walls'''
     def __init__(self, ms=100):
+        '''Initialize this map with a set of border walls'''
         self.mapsize = ms
         self.walls = [[0,  0,  ms, 0],  \
                       [ms, 0,  ms, ms], \
                       [ms, ms, 0,  ms], \
                       [0,  ms, 0,  0]]
-        self.addWall([200,200],[800,800])
-        self.addWall([200,800],[800,200])
-    
-    # add a wall from point a to point b
-    def addWall(self, a, b):
-        self.walls.append(a+b)
 
-# player object containing important information about a player.
+    def addWall(self, a, b):
+        '''add a wall from point a to point b'''
+        self.walls.append(a+b)
+    
+    
 class Player:
+    '''Player object containing important information about a player.'''
     def __init__(self, location, username, speed=8):
+        '''Setup the player object'''
         # for multiplayer functions
         self.username = username
         self.address = None
@@ -84,20 +86,24 @@ class Player:
         self.size = 10
 
     def __str__(self):
+        '''Return a stringified copy of this player'''
         return "username:"+self.username+", location:"+str(self.location)+", inputs:"+str(self.inputs)
 
-    # an event is passed to the client
-    # when an arrow key is pressed or released, the inputs variable is updated with the current inputs
     def handle_event(self, event):
+        '''an event is passed to the client
+        
+        when an arrow key is pressed or released, the inputs variable is updated with the current inputs'''
         if event.type in [pygame.KEYDOWN, pygame.KEYUP] and event.key in CLIENT_ARROW_KEYS:
             self.inputs = set()
             keys = pygame.key.get_pressed()
             for key in CLIENT_ARROW_KEYS:
                 if keys[key]:
                     self.inputs.add(CLIENT_TO_PROTOCOL[key])
-        
-    # given a set of walls, check the inputs variable and apply a change to the position of this player
+
     def update_location(self,walls):
+        '''Updates the location of this player
+        
+        Given a set of walls and player inputs, apply a change to the position of this player'''
         # calculate the desired change in position, (dx,dy)
         dx = dy = 0
         for i in self.inputs:
@@ -115,8 +121,11 @@ class Player:
         if self.role == 'ghost' or ((dy != 0 or dx != 0) and not self.wallcollide(self.location+newspot,walls)):
             self.location = newspot
 
-    # check if a step intersects a wall
     def wallcollide(self, step_vector, walls):
+        '''Check if a step intersects a wall
+
+        using math from: https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection'''
+
         x1,y1,x2,y2 = step_vector 
         for wall in walls:
             x3,y3,x4,y4 = wall 
@@ -135,13 +144,13 @@ class Player:
 
         return False
     
-    # get a rect representing the player, used for drawing
     def get_rect(self):
+        '''get a rect representing the player, used for drawing'''
         rect = pygame.Rect(0,0,self.size,self.size)
         rect.center = tuple(self.location)
         return rect
 
-# Base Game 
+ 
 class Game:
     '''Base Game class'''
     def __init__(self):
@@ -169,6 +178,7 @@ class Game:
         # print(self.player.location)
     
     def get_caught(self, seeker):
+        '''get a list of all hiding players that collide with the seeker'''
         seeker_rect = seeker.get_rect()
         caught = []
         for player in self.players:
@@ -183,22 +193,30 @@ class Game:
 
 
 class VisualGame(Game):
+    '''A class for the client-side of the hide and seek game
+    
+    Creates a GUI with pygame and maintains a UDP communication with the server
+    To use this object: initialize it, call login(), then call main_loop()'''
     def __init__(self, username):
+        '''Setup VisualGame object
+        
+        Sets up the pygame visualization and the UDP socket'''
         # pygame window setup 
         os.environ['SDL_VIDEO_CENTERED'] = '1'
         # os.environ["SDL_VIDEODRIVER"] = "dummy"
         pygame.init()
         pygame.display.set_caption(CAPTION)
         pygame.display.set_mode(SCREEN_SIZE)
-        
+        # call the super constructor
         Game.__init__(self)
+
         #init screen 
         self.screen = pygame.display.get_surface()
         self.screen.set_colorkey((0,0,0))
-
+        # initialize the Font
         self.textfont = pygame.font.Font(None, 25)
 
-        # create the player objects
+        # create the player objects - self.player is THIS client player, self.players is a list of all players
         self.player = Player([301,401], username)
         self.players = [self.player]
         
@@ -212,48 +230,56 @@ class VisualGame(Game):
         t.setDaemon(True) # don't hang on exit
         t.start()
 
-        self.recent_timestamp = 0
-    
-    def serve_forever(self):
-        while True:
-            data, address = self.socket.recvfrom(1024)
-            
-            # print('\treceived: %s bytes from %s' % (len(data), address))
-            # print('\treceived: %s' % data)
-            # print('\tunpickled:%s' % pickle.loads(data))
-            # print()
+        # current time_stamp will handle the 
+        self.current_update_timestamp = 0
 
-            
+    def serve_forever(self):
+        '''loop forever to handle incoming UDP packets'''
+        while True:
+            # read the data and address
+            data, address = self.socket.recvfrom(1024)
             
             if data:
                 data = pickle.loads(data)
-                
-                print('\tparsing data...')
 
+                print('received data: %s' % data['type'])
+
+                # if login fails, then quit. 
                 if data['type'] == 'login_ack':
                     print("successful login at address: %s:%d" % address)
                     if data['status'] != 'ok':
                         print("login failed")
-                        sys.exit(0)
-                # if inputs ack is not the current inputs, then send the current inputs again. 
-                if data['type'] == 'inputs_ack' and data['inputs'] != self.player.inputs:
-                    msg = pickle.dumps({'type':'inputs','inputs':self.player.inputs})
-                    self.send(msg)
+                        self.done = True 
+                        sys.exit(1)
+                # server may kick any players for inactivity or for failing to login properly
                 if data['type'] == 'kick':
                     print('kicked from game...')
-                    pygame.quit()
-                    thread.interrupt_main()
-                    sys.exit()
-                if data['type'] == 'update' and data['timestamp'] > self.recent_timestamp:
+                    self.done = True
+                    sys.exit(0)
+                # apply game-state updates
+                if data['type'] == 'update' and data['timestamp'] > self.current_update_timestamp:
+                    
+                    # save the current inputs, we don't want to change these based on server update. 
+                    current_inputs = self.player.inputs
+
+                    # get the players data from the server and get the client player in self.player
                     self.players = data['players']
                     self.player = next(x for x in data['players'] if x.username == self.player.username)
 
-                    self.state = data['game_state']
-
-                    self.recent_timestamp = data['timestamp']
+                    # if a server update contains incorrect player inputs, resend the inputs
+                    if current_inputs != self.player.inputs:
+                        print('correcting inputs to: %s' % str(current_inputs))
+                        self.send({'type':'inputs','inputs':current_inputs})
                     
-                
-                    # future: Add averaging or some other method to predict other player positions better
+                    # set the player inputs back to what the player is actually pressing
+                    self.player.inputs = current_inputs
+
+                    # update the game state
+                    self.state = data['game_state']
+                    # keep track of when the latest server update was received
+                    self.current_update_timestamp = data['timestamp']
+
+                    # TODO: Add averaging or some other method to reduce other players jumpiness
                     # now = dict()
                     # for p in self.players:
                     #     now[p.username] = p
@@ -262,12 +288,20 @@ class VisualGame(Game):
                     #         now[p.username] = p 
                     #     else:
                     #         self.players.append(p)
-                
+
     def send(self, data):
-        self.socket.sendto(data, SERVER_ADDRESS)
+        '''send data to the server
+
+        expects a dictionary. Will attach the timestamp and pickle the message'''
+
+        data['timestamp'] = int((time.time()-self.start_time)*10)
+        msg = pickle.dumps(data)
+        self.socket.sendto(msg, SERVER_ADDRESS)
         
     def login(self, username):
-        self.send(pickle.dumps({'type':'login', 'username':username, 'return_port':self.socket.getsockname()[1]}))
+        '''send a login message to the server with the username FIXME DONT SEND PORT NUM'''
+
+        self.send({'type':'login', 'username':username, 'return_port':self.socket.getsockname()[1]})
         print('waiting for server ack and map info...')
     
     def event_loop(self):
@@ -282,9 +316,7 @@ class VisualGame(Game):
                 self.player.handle_event(event)
 
                 # send a message to the server with updated player inputs
-                timestamp = int((time.time()-self.start_time)*10)
-                msg = pickle.dumps({'type':'inputs','inputs':self.player.inputs, 'timestamp':timestamp})
-                self.send(msg)
+                self.send({'type':'inputs','inputs':self.player.inputs})
     
     def draw(self):
         '''draw all elements to the display surface'''
@@ -378,9 +410,11 @@ class VisualGame(Game):
             self.draw()
             self.clock.tick(self.fps)
 
+
 class HeadlessGameServer(Game):
-    
+    '''Class for creating the hide-and-seek game server'''
     def __init__(self):
+        '''Setup the server and Game'''
         # initialize game attrs
         Game.__init__(self)
 
@@ -388,12 +422,13 @@ class HeadlessGameServer(Game):
         self.players = []
         self.seeker = None
 
+        # create the socket used for the server
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.client_addresses = set()
         
     
     # create UDP Server
     def setup_server(self):
+        '''Bind the server to the serverport and start the input and notification threads'''
         self.socket.bind((socket.gethostbyname(socket.getfqdn()),SERVER_ADDRESS[1]))
         print("starting server at %s:%d" % self.socket.getsockname())
         
@@ -406,34 +441,26 @@ class HeadlessGameServer(Game):
         t2.setDaemon(True) # don't hang on exit
         t2.start()
     
-    # what to do when packet received: 
-    #   update player info, 
-    #   send back ack
-    #   broadcast death/player join? 
     def serve_forever(self):
+        '''Receive and handle packets from the UDP Port'''
         while True:
             try:
                 data, address = self.socket.recvfrom(1024)
-            
-                # print('\treceived: %s bytes from %s' % (len(data), address))
-                # print('\treceived: %s' % data)
-                # print('\tunpickled:%s' % pickle.loads(data))
-                # print()
-                
                 if data:
-                    # print('\tparsing data...')
                     self.parse_data(data, address)
             except ConnectionResetError:
-                # do something here to recover when a client is lost
-                # self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                # print(socket.getfqdn())
-                # print(socket.gethostbyname(socket.getfqdn()))
-                # self.socket.bind((socket.gethostbyname(socket.getfqdn()),SERVER_ADDRESS[1]))
+                # TODO do something here to recover when a client is lost
                 print("connection lost")
+                pass
     
     def parse_data(self, data, address):
+        '''Parse and handle a packet
+        
+        Set the player inputs, handle a login, and reply if necessary'''
+        # unpickle the data
         data = pickle.loads(data)
         
+        # handle client login
         if data["type"] == "login":
             # username already taken
             if [x for x in self.players if x.username == data['username']]:
@@ -447,12 +474,9 @@ class HeadlessGameServer(Game):
                 newPlayer.address = address
                 newPlayer.last_input = -1
                 self.players.append(newPlayer)
-
-                self.client_addresses.add((address[0],data['return_port']))
                 
-
+        # handle client inputs
         if data["type"] == "inputs":
-            
             # get the Player Object for the player
             client_player = [x for x in self.players if x.address == address]
 
@@ -472,6 +496,7 @@ class HeadlessGameServer(Game):
         
     
     def notify_clients(self):
+        '''Send gamestate information to all clients at intervals'''
         start = time.time()
         while True:
             # send along a timestamp! the client will only apply the most recent timestamp update
@@ -489,7 +514,9 @@ class HeadlessGameServer(Game):
             
             
     def main_loop(self):
-        '''main loop'''
+        '''Main control loop
+        
+        Run the game simulation and handle game state changes'''
 
         # reset_time is the time when a round ends or the server starts
         reset_time = time.time()
@@ -524,6 +551,7 @@ class HeadlessGameServer(Game):
             self.clock.tick(self.fps)
 
     def round_start(self):
+        '''Start the round'''
         self.state="hiding"
         print("ROUND START...")
 
@@ -536,11 +564,13 @@ class HeadlessGameServer(Game):
         self.seeker.role = "seeker"
 
     def seeker_start(self):
+        '''Start the seeker phase'''
         self.state = "seeking"
         print("SEEKER START...")
 
 
     def round_end(self):
+        '''End the round'''
         self.state = "waiting"
         print("ROUND END...")
         for player in self.players:
